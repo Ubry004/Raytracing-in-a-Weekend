ae629896bf232f45a4c9791046ea0e6e3d5dfaf6
Kamron Ubry <kubry@ufl.edu>
2024-08-27 00:43:52 -0400

    Made a little smiley face

--- Ray Tracing/main.cpp
+++ Ray Tracing/main.cpp
@@ -6,50 +6,82 @@
 //#include "material.h"
 #include "sphere.h"
 
+const double PI = 3.14159;
+
 int main() {
     hittable_list world;
 
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+    auto ground_material = make_shared<lambertian>(color(0.7, 0.7, 0.7));
+    world.add(make_shared<sphere>(point3(0, -1015, 0), 1000, ground_material));
 
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
-            auto choose_mat = random_double();
-            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+    //for (int a = -11; a < 11; a++) {
+    //    for (int b = -11; b < 11; b++) {
+    //        auto choose_mat = random_double();
+    //        point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 
-            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-                shared_ptr<material> sphere_material;
+    //        if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+    //            shared_ptr<material> sphere_material;
 
-                if (choose_mat < 0.8) {
-                    // diffuse
-                    auto albedo = color::random() * color::random();
-                    sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }
-                else if (choose_mat < 0.95) {
-                    // metal
-                    auto albedo = color::random(0.5, 1);
-                    auto fuzz = random_double(0, 0.5);
-                    sphere_material = make_shared<metal>(albedo, fuzz);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }
-                else {
-                    // glass
-                    sphere_material = make_shared<dielectric>(1.5);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }
-            }
+    //            if (choose_mat < 0.8) {
+    //                // diffuse
+    //                auto albedo = color::random() * color::random();
+    //                sphere_material = make_shared<lambertian>(albedo);
+    //                world.add(make_shared<sphere>(center, 0.2, sphere_material));
+    //            }
+    //            else if (choose_mat < 0.95) {
+    //                // metal
+    //                auto albedo = color::random(0.5, 1);
+    //                auto fuzz = random_double(0, 0.5);
+    //                sphere_material = make_shared<metal>(albedo, fuzz);
+    //                world.add(make_shared<sphere>(center, 0.2, sphere_material));
+    //            }
+    //            else {
+    //                // glass
+    //                sphere_material = make_shared<dielectric>(1.5);
+    //                world.add(make_shared<sphere>(center, 0.2, sphere_material));
+    //            }
+    //        }
+    //    }
+    //}
+
+    //auto material1 = make_shared<dielectric>(1.5);
+    //world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    //auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    //world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    //auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    //world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    // Smiley Face
+
+    auto smile_material1 = make_shared<lambertian>(color(0.85, 0.13, 0.85));
+    auto smile_material2 = make_shared<dielectric>(1.5);
+
+    auto albedo = color::random(0.5, 1);
+    auto fuzz = random_double(0, 0.5);
+    auto smile_material3 = make_shared<metal>(albedo, fuzz);
+
+    // Render eyes
+    // Left eye
+    world.add(make_shared<sphere>(point3(0, 1, 1), 0.2, smile_material3));
+    // Right eye
+    world.add(make_shared<sphere>(point3(0, 1, -1), 0.2, smile_material3));
+
+    // Render smile
+    for (int i = 0; i <= 10; ++i) {
+        float t = (float)i / 10; // Interpolation value (0 to 1)
+        float angle = PI * t; // Angle for the mouth arc (semi-circle)
+        float z = -3.5 * t;
+        float y = 0 + sin(-angle);
+        if (i % 2 == 0) {
+            world.add(make_shared<sphere>(point3(0, y - 0.7, z + 1.75), 0.2, smile_material1));
         }
+        else {
+            world.add(make_shared<sphere>(point3(0, y - 0.7, z + 1.75), 0.2, smile_material3));
+        }
+        
     }
-
-    auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
-
-    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
-
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
     camera cam;
 
